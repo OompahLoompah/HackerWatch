f5291abbe277110306985073deedfe76a25fc9a6
Sean Heuer <sean@digitalrhomb.us>
2016-11-18 21:59:54 -0800

    Fleshed out the api and added a few new features

--- HackerWatch/HackerWatch.py
+++ HackerWatch/HackerWatch.py
@@ -1,18 +1,71 @@
+#!/usr/bin/python
+
 from api import api
-import time, json
+import optparse, time, json
+
+parser = optparse.OptionParser()
+parser.add_option('-t', '--top', help='Get the top stories', dest='top', default=False, action='store_true')
+parser.add_option('-s', '--show', help='Get show Hacker News stories', dest='show', default=False, action='store_true')
+parser.add_option('-j', '--jobs', help='Get job postings', dest='jobs', default=False, action='store_true')
+parser.add_option('-i', '--item', help='Get a single item', dest='item')
+parser.add_option('-u', '--user', help='Get user information', dest='user')
+
+(opts, args) = parser.parse_args()
 
 hn = api()
 
-story_ids = hn.PullTopStories()
+if opts.top:
+    story_ids = hn.GetTopStories()
 
-for story_id in story_ids:
-    story = hn.PullItem(story_id)
-    if story == int(1):
-        exit(1)
+    for story_id in story_ids:
+        story = hn.GetItem(story_id)
+        if story == int(1):
+            exit(1)
+        else:
+            try:
+                print story['title'].encode('ascii', 'ignore')
+            except UnicodeEncodeError, e:
+                print("\n\n\n\nWe choked on the following story:\n")
+                print story
+        time.sleep(1)
+
+if opts.show:
+    story_ids = hn.GetShowStories()
+
+    for story_id in story_ids:
+        story = hn.GetItem(story_id)
+        if story == int(1):
+            exit(1)
+        else:
+            try:
+                print story['title'].encode('ascii', 'ignore')
+            except UnicodeEncodeError, e:
+                print("\n\n\n\nWe choked on the following story:\n")
+                print story
+        time.sleep(1)
+
+if opts.jobs:
+    story_ids = hn.GetJobStories()
+
+    for story_id in story_ids:
+        story = hn.GetItem(story_id)
+        if story == int(1):
+            exit(1)
+        else:
+            try:
+                print story['title'].encode('ascii', 'ignore')
+            except UnicodeEncodeError, e:
+                print("\n\n\n\nWe choked on the following story:\n")
+                print story
+        time.sleep(1)
+
+if opts.item:
+    item = hn.GetItem(opts.item)
+    if item == int(1):
+        print('Invalid ID')
     else:
-        try:
-            print story['title'].encode('ascii', 'ignore')
-        except UnicodeEncodeError, e:
-            print("\n\n\n\nWe choked on the following story:\n")
-            print story
-    time.sleep(1)
+        print item
+
+if opts.user:
+    user = hn.GetUser(opts.user)
+    print user
--- HackerWatch/api.py
+++ HackerWatch/api.py
@@ -8,14 +8,14 @@ class api():
     def __init__(self):
         print("test1")
 
-    def PullTopStories(self):
+    def GetTopStories(self):
         try:
             stories = self.GetURL('https://hacker-news.firebaseio.com/v0/topstories.json')
             return stories
         except URLError, e:
             return int(1)
 
-    def PullItem(self, story_id):
+    def GetItem(self, story_id):
         try:
             item = self.GetURL('https://hacker-news.firebaseio.com/v0/item/' + str(story_id) + '.json')
             return item
@@ -27,6 +27,27 @@ class api():
             user = self.GetURL('https://hacker-news.firebaseio.com/v0/user/' + user_id + '.json')
             return user
         except URLError, e:
+            return int(1)
+
+    def GetJobStories(self):
+        try:
+            jobs = self.GetURL('https://hacker-news.firebaseio.com/v0/jobstories.json')
+            return jobs
+        except URLError, e:
+            return int(1)
+
+    def GetShowStories(self):
+        try:
+            show_stories = self.GetURL('https://hacker-news.firebaseio.com/v0/showstories.json')
+            return show_stories
+        except URLError, e:
+            return int(1)
+
+    def GetAskStories(self):
+        try:
+            ask_stories = self.GetURL('https://hacker-news.firebaseio.com/v0/askstories')
+            return ask_stories
+        except:
             return int(1)
 
     def GetURL(self, url):
